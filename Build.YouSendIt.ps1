0ebb48d616ecce89bf116332a03b05556808ef3f
Mark.Michaelis <Mark@IntelliTect.com>
2013-10-27 13:05:35 -0700

    Updated YouSendIt to the new name HighTail along with new exe; Updated PSake; Added Pester; Updated tests to use Pester a local source path

--- Build.YouSendIt.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    $taskList,
-    $parameters
-)
-if($parameters -eq $null) { $parameters = @{'packageDirectoryName' = 'YouSendIt'} }
-
-. "$(split-path $myInvocation.MyCommand.Definition)\build.ps1" -taskList $taskList -parameters $parameters
