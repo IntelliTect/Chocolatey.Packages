acfee356a86419aa5a00525c0382c191d22c75e0
Mark.Michaelis <Mark@IntelliTect.com>
2013-10-28 04:11:33 -0700

    Corrected path in log file for VisualStudioUltimate2013

--- VisualStudio2013Ultimate/Tools/chocolateyInstall.ps1
+++ VisualStudio2013Ultimate/Tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 if(!(Test-Path $Env:Temp\chocolatey\VisualStudio2013Ultimate)) {
 	New-Item $Env:Temp\chocolatey\VisualStudio2013Ultimate -ItemType Directory
 }
-install-chocolateypackage 'VisualStudioUltimate2013' 'exe' '/Passive /Full /Log %Temp%\chocolatey\VisualStudioUltimate2013\VisualStudioUltimate2013.log' 'http://download.microsoft.com/download/C/F/B/CFBB5FF1-0B27-42E0-8141-E4D6DA0B8B13/vs_ultimate.exe'
+install-chocolateypackage 'VisualStudioUltimate2013' 'exe' '/Passive /Full /Log $env:Temp\chocolatey\VisualStudioUltimate2013\VisualStudioUltimate2013.log' 'http://download.microsoft.com/download/C/F/B/CFBB5FF1-0B27-42E0-8141-E4D6DA0B8B13/vs_ultimate.exe'
