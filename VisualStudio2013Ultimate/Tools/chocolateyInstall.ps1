f6dafddd8b4146b08d1f580edc1ba983bd17e897
mark@IntelliTect.com <mark@IntelliTect.com>
2014-10-05 22:45:55 -0700

    Removed VisualStudio2013Ultimate as that is being maintained by someone else.

--- VisualStudio2013Ultimate/Tools/chocolateyInstall.ps1
+++
@@ -1,4 +0,0 @@
-if(!(Test-Path $Env:Temp\chocolatey\VisualStudio2013Ultimate)) {
-	New-Item $Env:Temp\chocolatey\VisualStudio2013Ultimate -ItemType Directory
-}
-install-chocolateypackage 'VisualStudio2013Ultimate' 'exe' "/Passive /Full /Log $env:Temp\chocolatey\VisualStudio2013Ultimate\VisualStudio2013Ultimate.log" 'http://download.microsoft.com/download/C/F/B/CFBB5FF1-0B27-42E0-8141-E4D6DA0B8B13/vs_ultimate.exe'
