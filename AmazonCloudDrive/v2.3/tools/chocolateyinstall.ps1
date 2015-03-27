fa0211076cd186c400e2db29cf9c44acefd099b5
Mark Michaelis <Mark@IntelliTect.com>
2015-03-26 22:39:30 -0700

    Added support for AmazonCloudDrive

---
+++ AmazonCloudDrive/v2.3/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+
+$packageName = 'AmazonCloudDrive' # arbitrary name for the package, used in messages
+$registryUninstallerKeyName = 'Amazon Cloud Drive' #ensure this is the value in the registry
+$installerType = 'EXE' #only one of these: exe, msi, msu
+
+function script:Get-AmazonCloudDriveInstallUrl($url) {
+    $content= $WebClient.DownloadString($url)
+    return [Regex]::Matches($content, $regex, "IgnoreCase") | 
+        %{ $_.Groups[1].value } | 
+            ?{ $_ -like "*AmazonCloudDriveSetup.exe" }
+}
+
+
+$url = (Get-AmazonCloudDriveInstallUrl "https://www.amazon.com/gp/drive/app-download")
+$url64 = '' # 64bit URL here or remove - if installer decides, then use $url
+$silentArgs = '/S' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" ["$url64"  -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
---
+++ AmazonCloudDrive/v2.3/tools/chocolateyuninstall.ps1
@@ -0,0 +1,49 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+# Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
+
+$packageName = 'AmazonCloudDrive'
+$registryUninstallerKeyName = 'Amazon Cloud Drive' #ensure this is the value in the registry
+$installerType = 'EXE'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+
+<#Private#> Function Script:Get-ProgramRegistryKeys {
+    [CmdletBinding()] param()
+
+    return [string] "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+                  "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+                  "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall",
+                  "Microsoft.PowerShell.Core\Registry::HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+}
+
+Function Get-Program {
+    [CmdletBinding()] param([string] $Filter = "*") 
+
+    Get-ChildItem (Get-ProgramRegistryKeys) | Get-ItemProperty | 
+            Select-Object  *,@{Name="Name"; Expression = { 
+                if( ($_ | Get-Member "DisplayName") -and $_.DisplayName) { #Consider $_.PSObject.Properties.Match("DisplayName") as it may be faster
+                    $_.DisplayName
+                } 
+                else { 
+                    $_.PSChildName 
+                } 
+            }} | 
+            ?{ ($_.Name -Like $Filter) -or ($_.PSChildName -Like $Filter)  } 
+}
+$amazonCloudDriveProgram = Get-Program "Amazon Cloud Drive"
+
+$file =$amazonCloudDriveProgram.UninstallString
+
+##$osBitness = Get-ProcessorBits
+#if ($osbitness -eq 64) {
+#  $file = (get-itemproperty -path "hklm:\software\wow6432node\microsoft\windows\currentversion\uninstall\$registryuninstallerkeyname").uninstallstring
+#} else {
+#  $file = (get-itemproperty -path "hklm:\software\microsoft\windows\currentversion\uninstall\$registryuninstallerkeyname").uninstallstring
+#} 
+	
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
