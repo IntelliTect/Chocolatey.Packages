3d8c5fcc7de9c1666c6c0a5624dffed89daaad40
Mark Michaelis <Mark@IntelliTect.com>
2015-10-17 12:43:08 -0700

    Removed individual build files in favor of using parameters.

--- Build.VS2013.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    $taskList,
-    $parameters
-)
-if($parameters -eq $null) { $parameters = @{'package' = 'VisualStudio2013Ultimate'} }
-
-. "$(split-path $myInvocation.MyCommand.Definition)\build.ps1" -taskList $taskList -parameters $parameters
