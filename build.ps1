6ac3c8e343818bc304be1f6f249c99fcff1fff98
Mark Michaelis <Mark@IntelliTect.com>
2015-10-18 07:19:12 -0700

    Added error reporting of choco pack command via a throw of the error message.

--- build.ps1
+++ build.ps1
@@ -1,22 +1,25 @@
 ﻿[CmdletBinding(DefaultParameterSetName="Package")]
 param(
     [ValidateScript({Test-Path $_ -PathType Leaf})][Parameter(Mandatory,ParameterSetName="nuspecFilePath")][string]$nuspecFilePath,
-    [ValidateScript({$_ -in (Get-ChildItem $PSScriptRoot -Exclude bin,build,.git -Directory).Name})][Parameter(Mandatory,ParameterSetName="Package")][string]$Package
+    [ValidateScript({$_.TrimStart(".\") -in (Get-ChildItem $PSScriptRoot -Exclude bin,build,.git -Directory).Name})][Parameter(Mandatory,ParameterSetName="Package")][string]$Package
 )
 
 Function Create-ChocolateyPackage {
-    [CmdletBinding()] 
+    [CmdletBinding(DefaultParameterSetName="Package")]
     param(
     [ValidateScript({Test-Path $_ -PathType Leaf})][Parameter(Mandatory,ParameterSetName="nuspec")][string]$nuspecFilePath,
-    [ValidateScript({$_ -in (Get-ChildItem $PSScriptRoot -Exclude bin,build,.git -Directory).Name})][Parameter(Mandatory,ParameterSetName="Package")][string]$Package
+    [ValidateScript({$_.TrimStart(".\") -in (Get-ChildItem $PSScriptRoot -Exclude bin,build,.git -Directory).Name})][Parameter(Mandatory,ParameterSetName="Package")][string]$Package
     )
 
     if($PsCmdlet.ParameterSetName -eq "Package") {
         $nuspecFilePath = (Get-ChildItem (Join-path $PSScriptRoot $Package) *.nuspec -Recurse)[-1] | Select-Object -ExpandProperty FullName
     }
 
-    Choco Pack $nuspecFilePath | %{ ` # -OutputDirectory doesn't appear to work, file is instead output to current directory. 
+    [bool]$packageCreatedSuccessfully=$false
+    [string]$packOutput = Choco Pack $nuspecFilePath 
+    $packOutput| %{ ` # -OutputDirectory doesn't appear to work, file is instead output to current directory. 
         if($_ -match "Successfully created package '(?'NugetFile'.*)'") {
+            $packageCreatedSuccessfully = $true
             Write-Output $Matches.NugetFile
         }
     } | %{
@@ -25,6 +28,8 @@ Function Create-ChocolateyPackage {
         Move-Item $_.FullName "$PSScriptRoot\bin\"  -Force  # Overwrite the file if it already exists
         Get-Item (Join-Path "$PSScriptRoot\bin\"  (Split-Path $_ -Leaf))  #Write the resulting nuget file to the output stream.
     }
+
+    if(!$packageCreatedSuccessfully) { Throw $packOutput.ToString() }
 }
 
 switch($PSCmdlet.ParameterSetName) {
